fd50604e3ba8ce0f56c4c93f90da7e4af544d7a2
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2018-12-01 14:42:38 +0100

    created dashboard pug template

--- public/javascript/pagination.js
+++
@@ -1,8 +0,0 @@
-$('#pagination-container').pagination({
-  dataSource: [1, 2, 3, 4, 5, 6, 7, ... , 195],
-  callback: function(data, pagination) {
-      // template method of yourself
-      var html = template(data);
-      $('#data-container').html(html);
-  }
-})
