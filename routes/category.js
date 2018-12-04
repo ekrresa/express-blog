bf1a9a15bba262a7ec7ee9805dccede7ef5cc62d
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2018-12-04 09:30:02 +0100

    installed joi package

--- routes/category.js
+++ routes/category.js
@@ -2,17 +2,25 @@ const Category = require("../models/Category");
 const express = require("express");
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  res.render("admin/index");
+});
+
 router.get("/category", async (req, res) => {
   res.render("admin/category");
 });
 
 router.post("/category", async (req, res) => {
+  if (!req.body.category) {
+    return res.render("admin/category", {
+      error: "category name is required"
+    });
+  }
   const category = new Category({
     name: req.body.category
   });
   await category.save();
-  res.redirect("index");
-  res.location("index");
+  res.redirect("/admin");
 });
 
 module.exports = router;
